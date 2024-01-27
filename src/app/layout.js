24e09a2c3f604c1b4f23cf59412445b5717d1b22
Diego Fernandes <fernandes.diego@academico.ifpb.edu.br>
2024-01-26 21:04:48 -0300

    first commit

---
+++ src/app/layout.js
@@ -0,0 +1,17 @@
+import { Inter } from "next/font/google";
+import "../styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Criação de Modal",
+  description: "Geração do Create Next",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
